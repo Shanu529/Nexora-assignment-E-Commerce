13c366c6b490f0f21f3509565c321a9dbc0b8a42
Shanu Chhetri <shnau.chhetri529@gmail.com>
2025-10-29 09:34:10 +0530

    .env

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://nexora-assignment-e-commerce-fronte.vercel.app/",
     credentials: true,
   })
 );
